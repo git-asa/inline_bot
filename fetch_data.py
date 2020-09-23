1bbc20d7430e09c31a6fda5ed5369e4498da04fe
v-zaferanieh <git.asa2.shared@gmail.com>
2020-09-23 14:56:17 +0330

    Sync Code from server

--- fetch_data.py
+++ fetch_data.py
@@ -2,25 +2,27 @@
 # -*- coding: UTF-8 -*-
 
 import mysql.connector
-from telegram import InlineQueryResultArticle, InputTextMessageContent, InputMediaPhoto, ParseMode
+from telegram import InlineQueryResultArticle, InputTextMessageContent, InputMediaPhoto, ParseMode, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 
 config = {
-    'user': 'root',
-    'password': '1234',
+    'user': 'amiuser',
+    'password': 'ERT*&^8052kL',
     'host': '127.0.0.1',
     'database': 'amibroker',
     'raise_on_warnings': True
 }
 
 
-def inputTextMessageContent(last_deal, close, open, high, low, clock,img):
+
+def inputTextMessageContent(last_deal, close, open, high, low, clock):
     message = '🔚<b>پایانی  </b>:{}\n\n'.format(close)
     message += '⬆<b>بیشترین </b>:{}\n\n'.format(high)
     message += '⬇<b>کمترین  </b>:{}\n\n'.format(low)
     message += '📈<b> آخرین  </b>:{}\n\n'.format(last_deal)
     message += '📉<b> اولین  </b>:{}\n\n'.format(open)
     message += '🕛️<b>آخرین اطلاعات قیمت</b>{}\n\n'.format(clock)
-    message += '{}\n\n'.format(img)
+    # message += '{}\n\n'.format(img)
     message += '<b> کانال رسمی </b>️\n\n'
 
     return message
@@ -29,37 +31,35 @@ def inputTextMessageContent(last_deal, close, open, high, low, clock,img):
 def get_data(var):
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor(dictionary=True)
-    query = "SELECT c.*, p1.* FROM symboles c JOIN symboles_data p1 ON (c.id = p1.symbol_id) LEFT OUTER JOIN symboles_data p2 ON (c.id = p2.symbol_id AND (p1.date < p2.date OR (p1.date = p2.date AND p1.id < p2.id))) WHERE p2.id IS NULL and  symbol LIKE %s LIMIT 50"
-    cursor.execute(query, (var + '%',))
+    query = "SELECT c.*, p1.* FROM symboles c LEFT JOIN symboles_data p1 ON (c.id = p1.symbol_id) LEFT OUTER JOIN symboles_data p2 ON (c.id = p2.symbol_id AND (p1.date < p2.date OR (p1.date = p2.date AND p1.id_symbloes_data < p2.id_symbloes_data))) WHERE p2.id_symbloes_data IS NULL and  symbol LIKE %s LIMIT 50"
+    cursor.execute(query, ('%'+var + '%',))
     symbol_list = cursor.fetchall()
     cursor.close()
     cnx.close()
     if not symbol_list:
         return
     results = list()
+    keyboard = [[InlineKeyboardButton("وضعیت صف", callback_data='1'),
+                 InlineKeyboardButton("حقیقی و حقوقی", callback_data='2')],
+
+                [InlineKeyboardButton("کدال", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
     for symbol in symbol_list:
+        # print(symbol)
+
         results.append(
             InlineQueryResultArticle(
                 id=symbol['id'],
                 title=symbol['symbol'],
-                input_message_content=InputTextMessageContent(message_text='twsr',parse_mode=ParseMode.HTML,disable_web_page_preview=False),
-                description=symbol[2],
-                thumb_url='https://vpn.amibroker.ir/static/telbot/images/' + str(symbol['id']) + '.jpg'
+                input_message_content=InputTextMessageContent(
+                    inputTextMessageContent(symbol['last_deal'], symbol['close'], symbol['open'], symbol['high'],
+                                            symbol['low'], symbol['time'])
+                                            , parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=False),
+                description=symbol['name'],
+                thumb_url='https://vpn.amibroker.ir/static/telbot/images/' + str(symbol['id']) + '.jpg',
+                reply_markup=reply_markup
             )
         )
     return results
-
-def get_symbol_data(var):
-    cnx = mysql.connector.connect(**config)
-    cursor = cnx.cursor(dictionary=True)
-    query = "SELECT * FROM symboles WHERE symbol LIKE %s LIMIT 50"
-    cursor.execute(query, (var + '%',))
-    symbol_list = cursor.fetchall()
-    cursor.close()
-    cnx.close()
-
-    ind = [650000, 560000]
-    lgl = [10000, 90000]
-    if not ind:
-        return
-    return [ind, lgl]
--- inline.py
+++ inline.py
@@ -1,9 +1,9 @@
-#git1
 from telegram.ext import Updater
 import logging
 from telegram.ext import InlineQueryHandler
-from fetch_data import get_data, get_symbol_data
-from plt import plot_individual_legal
+from fetch_data import get_data
+import time
+
 
 updater = Updater(token='1384477475:AAE2RYA7H-6kjXTY7B7XQwgRTMQ83arLwB0', use_context=True)
 dispatcher = updater.dispatcher
@@ -11,17 +11,31 @@ dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+
 def inline_symbol(update, context):
     var = update.inline_query.query
-    if len(var) > 1:
-        results = get_data(var)
-        ##
-        plot_individual_legal('پکرمان')
-        ##
-        context.bot.answer_inline_query(update.inline_query.id, results)
+    # if len(var) > 1:
+    results = get_data(var)
+    context.bot.answer_inline_query(update.inline_query.id, results, cache_time=10)
 
 
 inline_symbol_handler = InlineQueryHandler(inline_symbol)
 dispatcher.add_handler(inline_symbol_handler)
+############################vahid
 
+# def echo(update, context):
+#     for x in range(47,49):
+#         try:
+#             time.sleep(1)
+#             # print('https://vpn.amibroker.ir/static/telbot/images/'+str(x)+'.jpg')
+#             context.bot.send_photo(chat_id=update.effective_chat.id, caption= str(x), photo='https://vpn.amibroker.ir/static/telbot/images/'+str(x)+'.jpg')
+#         except AssertionError as error:
+#             print(error)
+#             print(str(x))
+
+
+from telegram.ext import MessageHandler, Filters
+# echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# ###########################vahid
+# dispatcher.add_handler(echo_handler)
 updater.start_polling()
