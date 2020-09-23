e2c7188636b1b5fff45404105200c833b0d01f07
git-asa <git.asa.shared@gmail.com>
2020-09-23 15:08:46 +0330

    add method "get_symbol_data"
    Signed-off-by: git-asa <git.asa.shared@gmail.com>

--- fetch_data.py
+++ fetch_data.py
@@ -14,7 +14,6 @@ config = {
 }
 
 
-
 def inputTextMessageContent(last_deal, close, open, high, low, clock):
     message = '🔚<b>پایانی  </b>:{}\n\n'.format(close)
     message += '⬆<b>بیشترین </b>:{}\n\n'.format(high)
@@ -32,7 +31,7 @@ def get_data(var):
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor(dictionary=True)
     query = "SELECT c.*, p1.* FROM symboles c LEFT JOIN symboles_data p1 ON (c.id = p1.symbol_id) LEFT OUTER JOIN symboles_data p2 ON (c.id = p2.symbol_id AND (p1.date < p2.date OR (p1.date = p2.date AND p1.id_symbloes_data < p2.id_symbloes_data))) WHERE p2.id_symbloes_data IS NULL and  symbol LIKE %s LIMIT 50"
-    cursor.execute(query, ('%'+var + '%',))
+    cursor.execute(query, ('%' + var + '%',))
     symbol_list = cursor.fetchall()
     cursor.close()
     cnx.close()
@@ -55,7 +54,7 @@ def get_data(var):
                 input_message_content=InputTextMessageContent(
                     inputTextMessageContent(symbol['last_deal'], symbol['close'], symbol['open'], symbol['high'],
                                             symbol['low'], symbol['time'])
-                                            , parse_mode=ParseMode.HTML,
+                    , parse_mode=ParseMode.HTML,
                     disable_web_page_preview=False),
                 description=symbol['name'],
                 thumb_url='https://vpn.amibroker.ir/static/telbot/images/' + str(symbol['id']) + '.jpg',
@@ -63,3 +62,18 @@ def get_data(var):
             )
         )
     return results
+
+def get_symbol_data(var):
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor(dictionary=True)
+    query = "SELECT * FROM symboles WHERE symbol LIKE %s LIMIT 50"
+    cursor.execute(query, (var + '%',))
+    symbol_list = cursor.fetchall()
+    cursor.close()
+    cnx.close()
+
+    ind = [650000, 560000]
+    lgl = [10000, 90000]
+    if not ind:
+        return
+    return [ind, lgl]
